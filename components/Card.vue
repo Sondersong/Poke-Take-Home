bc2198aabbc30534402d6df15f1eb31fa3be12d0
Sondersong <brianlgage@gmail.com>
2023-09-02 20:59:07 -0600

    chore: co for night

--- components/Card.vue
+++ components/Card.vue
@@ -5,5 +5,5 @@ const { data: pokeFacts } = await useFetch('/api/pokemonStats', { params: { name
 
 <template>
   <h1>{{ props.name }}</h1>
-  <img :src="pokeFacts.sprites.front_default"/>
+  <img :src="pokeFacts.sprites.front_default" />
 </template>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <script setup>
-  const searchInput = useState('searchInput')
+const searchInput = useState('searchInput')
 </script>
 <template>
   <h1>Poké-pedia</h1>
-  <input placeholder="Search" v-model="searchInput"/>
+  <input placeholder="Search" v-model="searchInput" />
 </template>
